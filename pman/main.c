8418fe052e7a85274c3d88c773d3e6ba25faa4a7
htmlJoel <ajmach6@gmail.com>
2022-02-10 09:52:21 -0600

    Initial Commit

---
+++ pman/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pman
+//
+//  Created by Joel Machens on 2/10/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
